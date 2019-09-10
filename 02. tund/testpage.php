81ca91a6de38c8a6b7337a365c741120cf02a79f
Anni-Liisi Hanni <annihan@users.noreply.github.com>
2019-09-10 11:34:24 +0300

    lisatud kellaaeg

--- 02. tund/testpage.php
+++ 02. tund/testpage.php
@@ -1,5 +1,12 @@
 <?php
-  $userName = "Anni-Liisi van der Hanni";
+  $userName = "Anni-Liisi Hanni"; 
+  $fullTimeNow = date("d.m.Y H:i:s"); 
+  $hourNow = date ("H"); 
+  $partOfDay = "hägune aeg"; 
+  
+  if($hourNow < 8){
+	  $partOfDay = "hommik";
+  }
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -17,5 +24,9 @@
     echo "<h1>" .$userName .", veebiprogrammeerimine</h1>"; 
   ?>
   <p>See veebileht on loodud õppetöö kaigus ning ei sisalda tõsiseltvõetavat sisu!</p>
+  <hr>
+  <?php
+  echo "<p>Lehe avamise hetkel oli aeg: " .$fullTimeNow . ", " . $partOfDay .".</p>"; 
+  ?>
 </body>
 </html>
