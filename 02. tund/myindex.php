d7e1ef41bad7455a26d094b3db679d4c6740d0c1
Anni-Liisi Hanni <annihan@users.noreply.github.com>
2019-09-10 11:09:15 +0300

    esimene üleslaadimine

---
+++ 02. tund/myindex.php
@@ -0,0 +1,19 @@
+<?php
+  //echo "Hei!";
+  phpversion();
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>Anni-Liisi Hanni programmeerib veebi</title>
+
+</head>
+<body>
+  <h1>Anni-Liisi Hanni, veebiprogrammeerimine</h1>
+  <p>See veebileht on loodud õppetöö kaigus ning ei sisalda tõsiseltvõetavat sisu!</p>
+  <hr>
+  <p> PHP serveri konfiguratsiooni infot näed <a href="serverinfo.php">siin<a/>. </p>
+  <p> Minu esimene programmeeritud leht on <a href="testpage.php">siin</a>.</p>
+</body>
+</html>
---
+++ 02. tund/testpage.php
@@ -0,0 +1,21 @@
+<?php
+  $userName = "Anni-Liisi van der Hanni";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+  echo $userName;
+  ?>
+   programmeerib veebi</title>
+
+</head>
+<body>
+  <?php
+    echo "<h1>" .$userName .", veebiprogrammeerimine</h1>"; 
+  ?>
+  <p>See veebileht on loodud õppetöö kaigus ning ei sisalda tõsiseltvõetavat sisu!</p>
+</body>
+</html>
